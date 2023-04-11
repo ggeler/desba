2d266f3cc887964bb299ef3a4e5c01d94837a147
Gaston Geler <ggeler@hotmail.com>
2023-04-11 11:43:30 -0300

    inicio proyecto

---
+++ src/main/java/ar/desba/DesbaApplication.java
@@ -0,0 +1,13 @@
+package ar.desba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/desba/DesbaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.desba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
