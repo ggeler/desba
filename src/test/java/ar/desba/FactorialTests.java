2fe4c8c61e6024b1b1ae692c1a185863f155097a
Gaston Geler <ggeler@hotmail.com>
2023-04-16 11:00:45 -0300

    Factorial 0
    factorial 1
    
    factorial N

--- src/test/java/ar/desba/FactorialTests.java
+++ src/test/java/ar/desba/FactorialTests.java
@@ -9,26 +9,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class FactorialTests {
 
 	@Test
-	public void PrueboFactorialRecursivo0Equals1() {
+	public void PrueboFactorial0Equals1() {
 		var fact = new Factorial();
-		assertThat(fact.factorialRecursivo(0)).isEqualTo(1L);
+		assertThat(fact.factorial(0)).isEqualTo(1L);
 	}
 	
 	@Test
-	public void PrueboFactorialRecursivo1Equals1() {
+	public void PrueboFactorial1Equals1() {
 		var fact = new Factorial();
-		assertThat(fact.factorialRecursivo(1)).isEqualTo(1L);
+		assertThat(fact.factorial(1)).isEqualTo(1L);
 	}
 	
 	@Test
-	public void PrueboFactorialRecursivo5Equals120() {
+	public void PrueboFactorial5Equals120() {
 		var fact = new Factorial();
-		assertThat(fact.factorialRecursivo(5)).isEqualTo(120L);
+		assertThat(fact.factorial(5)).isEqualTo(120L);
 	}
 	
 	@Test
-	public void PrueboFactorialRecursivo10Equals3628800() {
+	public void PrueboFactorial10Equals3628800() {
 		var fact = new Factorial();
-		assertThat(fact.factorialRecursivo(10)).isEqualTo(3628800L);
+		assertThat(fact.factorial(10)).isEqualTo(3628800L);
 	}
 }
